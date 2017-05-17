2380003ff5ef7f0f81e41f07e1562f882403f1ad
zoulinlin <lc999666333@163.com>
2017-05-17 16:55:49 +0800

    add proj

---
+++ YHFresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHFresh
+//
+//  Created by zoulinlin on 2017/5/17.
+//  Copyright © 2017年 AnyOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
